3aeb6fc20ad094849fdb62a953d16aca5a8e03d5
Ion Stratan <ion.stratan@luminoslabs.com>
2021-05-18 14:53:51 +0300

    Init commit

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,14 @@
 <template>
-  <div>Hello {{ value }}</div>
+    <div>Hello from the Azure Static Web App</div>
 </template>
 
 <script>
 export default {
-  name: "App",
-  data() {
-    return {
-      value: "World"
-    };
-  }
+    name: "App",
+    data() {
+        return {
+            value: "World",
+        };
+    },
 };
 </script>
